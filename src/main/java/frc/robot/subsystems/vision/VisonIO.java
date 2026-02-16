02f20175dcd455aa2acbdf7079bbcc3f84f2055a
Zachary Taylor <zacharytaylor0508@gmail.com>
2026-02-16 13:44:16 -0600

    init vision

---
+++ src/main/java/frc/robot/subsystems/vision/VisonIO.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems.vision;
+
+public interface VisonIO {
+
+    default void setVisionNeutral() {
+    }
+
+    default void trackingStart() {
+    }
+
+    default void periodic() {
+    }
+
+}
